343e3d5ac8616584aba1936f5eaf887f0b2e4e0a
Rahul Haque <rahulhaque07@gmail.com>
2020-05-20 01:21:30 +0600

    Calendar view transaction detail on click added

--- src/app/calendar/TransactionCalendar.jsx
+++ src/app/calendar/TransactionCalendar.jsx
@@ -5,59 +5,112 @@ import { Messages } from 'primereact/messages';
 import { Card } from 'primereact/card';
 import { FullCalendar } from 'primereact/fullcalendar';
 import { ProgressSpinner } from 'primereact/progressspinner';
+import { Dialog } from 'primereact/dialog';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
 
-import { reportApiEndpoints } from './../../API';
+import ExpenseListItem from './../expense/ExpenseListItem';
+
+import { expenseApiEndpoints, reportApiEndpoints, incomeApiEndpoints } from './../../API';
 import axios from './../../Axios';
 
 let messages;
-
-const options = {
-  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin],
-  defaultView: 'dayGridMonth',
-  themeSystem: 'standard',
-  height: 'auto',
-  titleFormat: { year: 'numeric', month: 'long' },
-  buttonText: {
-    today: 'Today',
-    month: 'Month',
-    week: 'Week',
-    day: 'Day',
-    list: 'List'
-  },
-  header: {
-    left: 'dayGridMonth,listWeek', // timeGridWeek,timeGridDay
-    center: 'title',
-    right: 'today,prev,next' // prevYear,nextYear
-  },
-  editable: false,
-  // dateClick: (info) => {
-  //   console.log('Clicked on: ' + info.dateStr);
-  //   console.log('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-  //   console.log('Current view: ' + info.view.type);
-  // },
-  eventClick: (info) => {
-    console.log(info.event);
-    console.log('Event: ' + info.event.id);
-    console.log('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-    console.log('View: ' + info.view.type);
-
-    // change the border color just for fun
-    info.el.style.borderColor = 'red';
-  },
-};
 
 const TransactionCalendar = (props) => {
 
   const [fetching, setFetching] = useState(true);
   const [events, setEvents] = useState([]);
+  const [transactionsByDate, setTransactionByDate] = useState({ transactions: [], transactionsLoading: true });
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     requestTransaction();
   }, []);
+
+  const options = {
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin],
+    defaultView: 'dayGridMonth',
+    themeSystem: 'standard',
+    height: 'auto',
+    titleFormat: { year: 'numeric', month: 'long' },
+    buttonText: {
+      today: 'Today',
+      month: 'Month',
+      week: 'Week',
+      day: 'Day',
+      list: 'List'
+    },
+    header: {
+      left: 'dayGridMonth,listWeek', // timeGridWeek,timeGridDay
+      center: 'title',
+      right: 'today,prev,next' // prevYear,nextYear
+    },
+    editable: false,
+    // dateClick: (info) => {
+    //   console.log('Clicked on: ' + info.dateStr);
+    //   console.log('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+    //   console.log('Current view: ' + info.view.type);
+    // },
+    eventClick: (info) => {
+      requestTransactionDetail(`${info.event.id}%`, info.event.extendedProps.type);
+      setModalVisible(true);
+    },
+  };
+
+  const renderTransactionDetails = () => {
+    if (transactionsByDate.transactionsLoading) {
+      return (
+        <div className="p-grid p-nogutter p-justify-center">
+          <ProgressSpinner style={{ height: '25px' }} strokeWidth={'4'} />
+        </div>
+      );
+    }
+    else {
+      if (transactionsByDate.transactions.length > 0) {
+        return transactionsByDate.transactions.map((item, index) => {
+          return <ExpenseListItem key={item.id} itemDetail={item} />;
+        })
+      }
+      else {
+        return (
+          <div className="p-grid p-nogutter p-justify-center">
+            <h4 className="color-subtitle">Failed to retrieve data.</h4>
+          </div>
+        );
+      }
+    }
+  };
+
+  const requestTransactionDetail = async (transaction_date, transaction_type) => {
+
+    let api_endpoint = '';
+
+    if (transaction_type === 'Income') {
+      api_endpoint = incomeApiEndpoints.income;
+    }
+    else {
+      api_endpoint = expenseApiEndpoints.expense;
+    }
+
+    await axios.get(api_endpoint + '?per_page=5&sort_order=desc&search_col=transaction_date&search_by=' + transaction_date, {})
+      .then(response => {
+        console.log(response.data);
+        setTransactionByDate({
+          ...transactionsByDate,
+          transactions: response.data.data,
+          transactionsLoading: false
+        });
+      })
+      .catch(error => {
+        console.log('error', error);
+        setTransactionByDate({
+          ...transactionsByDate,
+          transactionsLoading: false
+        });
+      });
+  };
 
   const requestTransaction = () => {
     axios.get(reportApiEndpoints.transaction, {})
@@ -72,12 +125,14 @@ const TransactionCalendar = (props) => {
                   id: item.formatted_date,
                   title: `(+) ${item.total} ${item.currency_name}`,
                   date: item.formatted_date,
+                  type: item.transaction_type,
                   backgroundColor: '#4caf50',
                   borderColor: '#3c9c40',
                 } : {
                   id: item.formatted_date,
                   title: `(-) ${item.total} ${item.currency_name}`,
                   date: item.formatted_date,
+                  type: item.transaction_type,
                   backgroundColor: '#f47036',
                   borderColor: '#f45b36'
                 }
@@ -103,6 +158,24 @@ const TransactionCalendar = (props) => {
           </div>
         </div>
       </div>
+
+      <Dialog
+        header="Transaction Detail"
+        visible={modalVisible}
+        style={{ width: '80%' }}
+        modal={true}
+        onHide={() => {
+          setTransactionByDate({
+            ...transactionsByDate,
+            transactions: [],
+            transactionsLoading: false
+          });
+          setModalVisible(false);
+        }}
+        dismissableMask={true}
+      >
+        {renderTransactionDetails()}
+      </Dialog>
 
       <div className="p-grid">
 
