d8bfbdf390621ae248d86863a284efffaa91a3a5
Rahul Haque <rahulhaque07@gmail.com>
2020-07-30 00:11:58 +0600

    Dashboard recent income expense date format fix

--- src/app/expense/ExpenseListItem.jsx
+++ src/app/expense/ExpenseListItem.jsx
@@ -10,7 +10,7 @@ const ExpenseListItem = (props) => {
       <div>
         <div className="" style={{ display: 'flex', justifyContent: 'space-between', fontWeight: 'bold', fontSize: 16 }}>{itemDetail.spent_on}<div className="color-title">{itemDetail.amount.toLocaleString()} {itemDetail.currency_code}.</div></div>
         <div className="color-link" style={{ fontSize: 12 }}>{itemDetail.category_name}</div>
-        <div className="color-title" style={{ fontSize: 12 }}>{dayjs(itemDetail.expense_date).format('YYYY-MM-DD hh:mma')}</div>
+        <div className="color-title" style={{ fontSize: 12 }}>{dayjs(itemDetail.transaction_date).format('YYYY-MM-DD hh:mma')}</div>
       </div>
     </Card>
   );
--- src/app/income/IncomeListItem.jsx
+++ src/app/income/IncomeListItem.jsx
@@ -9,7 +9,7 @@ const IncomeListItem = (props) => {
       <div>
         <div className="" style={{ display: 'flex', justifyContent: 'space-between', fontWeight: 'bold', fontSize: 16 }}>{itemDetail.source}<div className="color-title">{itemDetail.amount.toLocaleString()} {itemDetail.currency_code}.</div></div>
         <div className="color-link" style={{ fontSize: 12 }}>{itemDetail.category_name}</div>
-        <div className="color-title" style={{ fontSize: 12 }}>{dayjs(itemDetail.income_date).format('YYYY-MM-DD hh:mma')}</div>
+        <div className="color-title" style={{ fontSize: 12 }}>{dayjs(itemDetail.transaction_date).format('YYYY-MM-DD hh:mma')}</div>
       </div>
     </Card>
   );
