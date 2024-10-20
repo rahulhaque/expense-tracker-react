7f08e7407bf1db610d1029aecff578fe0bde4ac8
Rahul <rahulhaque07@gmail.com>
2024-10-21 01:21:59 +0600

    Code cleanup 🧹

--- src/app/dashboard/AppTopbar.jsx
+++ src/app/dashboard/AppTopbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ToolsSidebar from './../common/ToolsSidebar';
-import { Link } from 'react-router-dom';
 
 const AppTopbar = (props) => {
 
